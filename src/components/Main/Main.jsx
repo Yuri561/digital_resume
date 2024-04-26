0ff0fbfd98ddb3c47aa958b73f22a7ee583e8516
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-04-26 08:32:28 -0400

    dark mode added and date() added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,13 +4,19 @@ import WordOfTheDay from '../WordOfTheDay/WordOfTheDay';
 
 
 const Main = () => {
+  const time = new Date();
+  const format = time.toLocaleString('en-US', {
+    hour: 'numeric', minute: 'numeric', month: 'short', day: 'numeric', hour12: true });
   return (
     <div className='main-wrapper w-100 d-flex flex-column justify-content-center align-items-center'>
     <div className='dashboard-container w-100 d-flex flex-column'>
       <h1 className='title text-center'>Dashboard</h1>
       <div className="box-group d-flex justify-content-center">
-        <div className="box"id='box1'>
+        <div className="box flex-column"id='box1'>
         <i className="bi bi-sun-fill"></i>
+        <h5 className='h1 p-2'>{format.toString()}</h5>
+
+        
         </div>
         <div className="box"id='box2'></div>
         <div className="box"id='box3'></div>
