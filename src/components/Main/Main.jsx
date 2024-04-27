9d65ca28bb937139378da0a685af60baff937b3d
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-04-26 22:39:31 -0400

    added music player functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -40,12 +40,15 @@ const Main = () => {
   }, []); // Run only once when the component mounts
 
   return (
-    <div className='main-wrapper w-100 d-flex flex-column justify-content-center align-items-center'>
-      <div className='dashboard-container w-100 d-flex flex-column'>
-        <h1 className='title text-center mt-5'>Dashboard</h1>
-        <span className='text-center typewriter h4'>
+    <div className="main-wrapper w-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="dashboard-container w-100 d-flex flex-column">
+        <h1 className="title text-center mt-5">Dashboard</h1>
+        <span className="text-center typewriter h4">
           <Typewriter
-            words={['Hello welcome home is a pleasure to have you back',  'hover over Vera to get started!!']}
+            words={[
+              "Hello welcome home is a pleasure to have you back",
+              "hover over Vera to get started!!",
+            ]}
             loop={0}
             cursor
             typeSpeed={50}
@@ -57,35 +60,38 @@ const Main = () => {
         </span>
 
         <div className="box-group d-flex justify-content-center">
-          <div className="box flex-column" id='box1'>
+          <div className="box flex-column" id="box1">
             <i className="bi bi-sun-fill"></i>
-            <h5 className='h1 p-2'>{format.toString()}</h5>
+            <h5 className="h1 p-2">{format.toString()}</h5>
           </div>
-          <div className="box" id='box2'>
-          <iframe width="100%"
-          height="100%"
-          className='youtube'
-          src={`https://www.youtube.com/embed/${randomVideo}?autoplay=1&mute=1&controls=1`}
-          title="News Video"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen/>
-          
+          <div className="box" id="box2">
+            <iframe
+              width="100%"
+              height="100%"
+              className="youtube"
+              src={`https://www.youtube.com/embed/${randomVideo}?autoplay=1&mute=1&controls=1`}
+              title="News Video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
           </div>
-          <div className="box" id='box3'>
-            <div className='btn-hover w-100 justify-content-center mb-5'>
-              <div className=' icon-container border-radius-2'>
-               <span className="bi bi-mic" id='icon'></span> 
+          <div className="box" id="box3">
+            <div className="btn-hover w-100 justify-content-center mb-5">
+              <div className=" icon-container border-radius-2">
+                <span className="bi bi-mic" id="icon"></span>
               </div>
             </div>
           </div>
-          <div className="box" id='box4'>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar />
-        </LocalizationProvider>
+          <div className="box" id="box4">
+            <div className="calendar-content">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar className="calendar" />
+              </LocalizationProvider>
+            </div>
           </div>
         </div>
-        <WordOfTheDay/>
+        <WordOfTheDay />
       </div>
     </div>
   );
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,42 +4,114 @@ import React from 'react';
 import './Sidebar.css';
 import Logo from '../../assets/img.jpeg';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+
+import afroHouse from '../../img/afroHouse.png';
 
 
 const Sidebar = () => {
+  const theme = useTheme();
   return (
-    <div className="card text-center" style={{ width: '18rem' }}>
+    <div className="card text-center" style={{ width: "18rem" }}>
       <div className="card-body">
-        <img src={Logo} className='card-img-top img' alt="" />
+        <img src={Logo} className="card-img-top img" alt="" />
         <h5 className="card-title">Yuri Pierre-Louis</h5>
         <p className="card-text">Aspiring Full-Stack Developer</p>
-        <a href="#" className="btn btn-primary">Connect</a>
+        <a href="#" className="btn btn-primary">
+          Connect
+        </a>
 
         <div className="list-group mt-3 gap-3 text-light">
-          <li type="button" className="list-group-item list-group-item-action" aria-current="true">
-          <i className="bi bi-house-fill icon"></i>
+          <li
+            type="button"
+            className="list-group-item list-group-item-action"
+            aria-current="true"
+          >
+            <i className="bi bi-house-fill icon"></i>
             Dashboard
           </li>
-           {/* Corrected here */}
-          <li  className="list-group-item list-group-item-action ">
-          <i className="bi bi-building icon"></i>Education</li>
-          <li type="button"
-           className="list-group-item list-group-item-action">
-          <i className="bi bi-card-list icon"></i>Projects</li>
+          {/* Corrected here */}
           <li className="list-group-item list-group-item-action ">
-          <i className="bi bi-person-badge icon"></i>About</li>
-          <li  className="list-group-item list-group-item-action">Hobbies</li>
+            <i className="bi bi-building icon"></i>Education
+          </li>
+          <li type="button" className="list-group-item list-group-item-action">
+            <i className="bi bi-card-list icon"></i>Projects
+          </li>
+          <li className="list-group-item list-group-item-action ">
+            <i className="bi bi-person-badge icon"></i>About
+          </li>
+          <li className="list-group-item list-group-item-action">Hobbies</li>
         </div>
-
+        <div className='music-container mt-5'>
+        
+        
+        
+        <Card sx={{ display: "flex" }}>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography component="div" variant="h5">
+                Live From Space
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                Mac Miller
+              </Typography>
+            </CardContent>
+            <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+              <IconButton aria-label="previous">
+                {theme.direction === "rtl" ? (
+                  <SkipNextIcon />
+                ) : (
+                  <SkipPreviousIcon />
+                )}
+              </IconButton>
+              <IconButton aria-label="play/pause">
+                <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+              </IconButton>
+              <IconButton aria-label="next">
+                {theme.direction === "rtl" ? (
+                  <SkipPreviousIcon />
+                ) : (
+                  <SkipNextIcon />
+                )}
+              </IconButton>
+            </Box>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 151}}
+            image={afroHouse}
+            alt=""
+          />
+        </Card>
+        </div>
       </div>
       <div className="card-footer">
         <div className="socials">
-            <ul className="list-group list-group-horizontal mb-5">
-                <a href='' className='list-group-item'><i className="bi bi-instagram"></i></a>
-                <a href='' className='list-group-item'><i class="bi bi-linkedin"></i></a>
-                <a href='' className='list-group-item'><i class="bi bi-github"></i></a>
-            </ul>
-         {/* Corrected here */}
+          <ul className="list-group list-group-horizontal mb-5">
+            <a href="" className="list-group-item">
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a href="" className="list-group-item">
+              <i class="bi bi-linkedin"></i>
+            </a>
+            <a href="" className="list-group-item">
+              <i class="bi bi-github"></i>
+            </a>
+          </ul>
+          {/* Corrected here */}
         </div>
       </div>
     </div>
