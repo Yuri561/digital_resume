2bf224438e45481bac75adf3309c7a124c093db2
Yuri <hazelirvin@gmail.com>
2024-04-29 23:39:55 -0400

    Committing local changes before merge

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, {useState} from 'react';
 import './Sidebar.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Profile from '../Profile/Profile';
 import Menu from '../Menu/Menu';
+import {motion} from 'framer-motion'
 import MusicPlayer  from '../MusicPlayer/MusicPlayer';
-
+import { UilBars } from "@iconscout/react-unicons";
+import { UilSignOutAlt } from "@iconscout/react-unicons";
 
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+
+  const [expanded, setExpaned] = useState(true)
+
+  const sidebarVariants = {
+    true: {
+      left : '0'
+    },
+    false:{
+      left : '-60%'
+    }
+  }
   return (
+    <>
     <aside
-      className="container text-center position-relative"
+      className="container text-center position-relative sidebar"
       style={{ width: "18rem" }}
     >
-      <button
-        className="btn position-absolute btn-primary close-btn"
-        type="button"
-      ></button>
+     <div className="bars" style={expanded?{left: '60%'}:{left: '5%'}} onClick={()=>setExpaned(!expanded)}>
+        <UilBars />
+      </div>
+    <motion.div className='sidebar'
+    variants={sidebarVariants}
+    animate={window.innerWidth<=768?`${expanded}`:''}
+    >
       <Profile />
 
       <div className="list-group mt-3 gap-3 text-light">
@@ -51,7 +69,12 @@ const Sidebar = () => {
           {/* Corrected here */}
         </div>
       </div>
+  
+     
+      </motion.div>
+      
     </aside>
+    </>
   );
 }
 
